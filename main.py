09f0ffb517fe8a55a7af3c7055106fcf6e07d7b0
strikeraryu <striker.aryu56@gmail.com>
2021-09-24 13:28:12 +0530

    env fix

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
 import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from src.CLI import CLI
 
+load_dotenv()
 
 cogs = [CLI]
 client = commands.Bot(command_prefix='-', intents = discord.Intents.all())
@@ -11,4 +12,4 @@ client = commands.Bot(command_prefix='-', intents = discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run(os.environ['BOT_TOKEN'])
+client.run(os.getenv('BOT_TOKEN'))
